086d0c6486638f845ccaa785e7ed25c7d1b7a4fa
Michael M <m.migsch@gmail.com>
2019-09-29 21:47:30 +0200

    Create brokenWall.py

---
+++ brokenWall.py
@@ -0,0 +1,25 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+import random
+
+def brokenBlock():
+    brokenBlocks = [48, 67, 4, 4, 4, 4]
+    block = random.choice(brokenBlocks)
+    return block
+
+pos = mc.player.getTilePos()
+x, y, z = pos.x, pos.y, pos.z
+
+brokenWall = []
+height, width = 5, 10
+
+brokenWall = [[brokenBlock() for _ in range(10)] for _ in range(10)]
+
+for row in brokenWall:
+    for block in row:
+        mc.setBlock(x, y, z, block)
+        x += 1
+    y += 1
+    x = pos.x
+
