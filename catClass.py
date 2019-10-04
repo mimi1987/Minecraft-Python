ea11d0378a15a7ea761f86bd3335dbaf4cdcb029
Michael M <m.migsch@gmail.com>
2019-10-04 21:32:56 +0200

    Update catClass.py

--- catClass.py
+++ catClass.py
@@ -4,6 +4,14 @@ class Cat(object):
         self.name = name
         self.weight = weight
 
+    def eat(self, food):
+        self.weight = self.weight + 0.05
+        print(self.name + " is eating " + food)
+
+    def eatAndSleep(self, food):
+        self.eat(food)
+        print(self.name + " is sleeping...")
+
         
 # Creating an instance of cat class
 fluff = Cat("Fluff", 4.5)
@@ -13,5 +21,14 @@ print(fluff.weight)
 
 # Changing the weight of fluff
 fluff.weight = 5
+print(fluff.weight)
+
+# Fluff is eating
+fluff.eat("tuna")
+
+# Fluff eats again and sleeps after it
+fluff.eatAndSleep("tuna")
+
+# Print the updated weight attribute
 print(fluff.weight)
 
